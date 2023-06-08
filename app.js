b5e2b44de27fb65bae3252a13aa173f7eb760e9d
Ahmed <dosunmuahmed240@gmail.com>
2023-06-08 20:21:28 +0100

    added the source and module files

---
+++ app.js
@@ -0,0 +1,11 @@
+var store = require('./store');
+
+console.log(store.adder(3, 5));
+
+console.log(store.counter(['shawn', 'Daniel', 'Bob']));
+
+// console.log(store.readMe('bill.txt'));
+
+// store.writeMe('say.pdf', 'bill.txt')
+
+// store.delFile('bill.txt');
---
+++ store.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+
+var adder = function (a, b) {
+    return `The sum of the two numbers is ${a + b}`;
+};
+
+var counter = function(arr){
+    return `The length of the array is ${arr.length}`;
+}
+
+var readMe = function(file){
+    return `${fs.readFileSync(file, 'utf8')}`;
+};
+
+var writeMe = function(file, source){
+    fs.readFile(source, 'utf8', function(err, data){
+        return `${fs.writeFileSync(file, data)}`;
+    }); 
+};
+
+var delFile = function(file){
+    fs.unlink(file, function(err, data){
+        return data;
+    })
+};
+
+var makeDir = function(dir){
+    fs.mkdir(dir, function(err, data){
+        return data;
+    })
+};
+
+module.exports = {
+    adder: adder,
+    counter: counter,
+    readMe: readMe,
+    writeMe: writeMe,
+    delFile: delFile,
+    makeDir: makeDir,
+}
+
